f08bd116c7a5b9130579f012f6c001a37e53defd
saisrinu <saisrinugampa135@gmail.com>
2024-07-23 20:47:23 +0530

    secured dajngotoken

--- bulkmailer/settings.py
+++ bulkmailer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-f-p@#$ii$5gr(^7+37fq4i^@m-)$xhu#-o_$^^%u5d2&6wa+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
